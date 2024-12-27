72764bd6df81eedab94408c5ce823e8c3f692bbe
kristof tops <kristof.tops@blueit.be>
2024-12-27 13:29:34 +0100

    Add looping logic to your code

--- 3 Add Logic to C# Console Applications/AddLogic/Program.cs
+++ 3 Add Logic to C# Console Applications/AddLogic/Program.cs
@@ -98,10 +98,9 @@ Console.WriteLine("-----------------------------\n");
 
 //------------------------------------------------------------------------------------------------
 // Code challenge - implement the FizzBuzz challenge rules
-//------------------------------------------------------------------------------------------------
 for (int i = 1; i <= 100; i++)
 {
-    var message = $"{i} ";
+    var message = $"{i}";
 
     if (i % 3 == 0 && i % 5 == 0)
         Console.WriteLine($"{i} - FizzBuzz");
@@ -115,4 +114,114 @@ for (int i = 1; i <= 100; i++)
     else 
         Console.WriteLine(i);
 }
+Console.WriteLine("-----------------------------\n");
+
+
+
+//------------------------------------------------------------------------------------------------
+// Role playing game battle challenge
+//------------------------------------------------------------------------------------------------
+Random attack = new();
+int heroHealth = 10, monsterHealth = 10, damage = 0;
+
+do
+{
+    damage = attack.Next(1, 11);
+    monsterHealth -= damage;
+    Console.WriteLine($"Monster was damaged and lost {damage} and now has {monsterHealth} health.");
+
+    if (monsterHealth <= 0)
+        break;
+    
+    damage = attack.Next(1, 11);
+    heroHealth -= damage;
+    Console.WriteLine($"Hero was damaged and lost {damage} and now has {heroHealth} health.");
+
+} while (heroHealth > 0 && monsterHealth > 0);
+
+Console.WriteLine(heroHealth > 0 ? "Hero wins!" : "Monster wins!");
+Console.WriteLine("-----------------------------\n");
+
+
+
+//------------------------------------------------------------------------------------------------
+// Code project 1 - write code that validates integer input
+//------------------------------------------------------------------------------------------------
+int inputParsed = 0;
+bool inputIsValid = false;
+
+Console.WriteLine("Enter an integer value between 5 and 10");
+
+do
+{
+    var input = Console.ReadLine();
+    inputIsValid = int.TryParse(input, out inputParsed);
+
+    if (!inputIsValid)
+    {
+        Console.WriteLine("Sorry, you entered an invalid number, please try again");
+        continue;
+    }
+
+    if (inputParsed < 5 || inputParsed > 10)
+    {
+        inputIsValid = false;
+        Console.WriteLine($"You entered {inputParsed}. Please enter a number between 5 and 10");
+        continue;
+    }
+} while (!inputIsValid);
+
+Console.WriteLine($"Your input value ({inputParsed}) has been accepted.");
+Console.WriteLine("-----------------------------\n");
+
+
+
+//------------------------------------------------------------------------------------------------
+// Code project 2 - write code that validates string input
+//------------------------------------------------------------------------------------------------
+string? inputRole = string.Empty;
+string[] roleOptions = ["administrator", "manager", "user"];
+bool inputRoleIsValid = false;
+
+Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+
+do
+{
+    inputRole = Console.ReadLine();
+    if (roleOptions != null && roleOptions.Contains(inputRole?.Trim().ToLower()))
+        inputRoleIsValid = true;
+    else
+        Console.WriteLine($"The role name that you entered, \"{inputRole}\" is not valid. Enter your role name (Administrator, Manager, or User)");
+    
+} while (!inputRoleIsValid);
+
+Console.WriteLine($"Your input value ({inputRole}) has been accepted.");
+Console.WriteLine("-----------------------------\n");
+
+
+
+//------------------------------------------------------------------------------------------------
+// Code project 2 - write code that validates string input
+//------------------------------------------------------------------------------------------------
+string[] myStrings = ["I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices"];
+int periodLocation = 0;
+int startingLocation = 0;
+
+foreach (var myString in myStrings)
+{
+    periodLocation = myString.IndexOf('.');
+    var content = myString;
+
+    while (periodLocation != -1)
+    {
+        var substring = content[..periodLocation].Trim();
+        Console.WriteLine(substring);
+
+        content = content.Remove(0, periodLocation + 1).Trim();
+        periodLocation = content.IndexOf('.');
+    }
+
+    Console.WriteLine(content);
+}
+
 Console.WriteLine("-----------------------------\n");
