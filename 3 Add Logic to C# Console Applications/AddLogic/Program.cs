13a9d151c8eb7e710570ad406448e76fcb66b86a
kristof tops <kristof.tops@blueit.be>
2024-12-27 15:04:05 +0100

    Guided project - Develop conditional branching and looping structures

--- 3 Add Logic to C# Console Applications/AddLogic/Program.cs
+++ 3 Add Logic to C# Console Applications/AddLogic/Program.cs
@@ -205,7 +205,6 @@ Console.WriteLine("-----------------------------\n");
 //------------------------------------------------------------------------------------------------
 string[] myStrings = ["I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices"];
 int periodLocation = 0;
-int startingLocation = 0;
 
 foreach (var myString in myStrings)
 {
