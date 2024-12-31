7b9a9f4e3979da0ea9d2b6602a33ea7b6b41ba19
kristof tops <kristof.tops@blueit.be>
2024-12-31 14:06:10 +0100

    Create and throw exceptions

--- 6 Debug C# Console Applications/Debug101/Program.cs
+++ 6 Debug C# Console Applications/Debug101/Program.cs
@@ -184,6 +184,75 @@ catch (DivideByZeroException ex)
 }
 
 Console.WriteLine("Exiting program.");
+Console.WriteLine("-----------------------------\n");
 
-Console.ReadLine();
+
+
+//------------------------------------------------------------------------------------------------
+// Exercise - Complete a challenge activity for creating and throwing exceptions
+//------------------------------------------------------------------------------------------------
+string[][] userEnteredValues = new string[][]
+{
+    ["1", "2", "3"],
+    ["1", "two", "3"],
+    ["0", "1", "2"]
+};
+
+try
+{
+    Workflow1(userEnteredValues);   
+    Console.WriteLine("'Workflow1' completed successfully.");
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine("An error occurred during 'Workflow1'.");
+    Console.WriteLine(ex.Message);
+    
+}
+
+static void Workflow1(string[][] userEnteredValues)
+{
+    foreach (string[] userEntries in userEnteredValues)
+    {
+        try
+        {
+            Process11(userEntries);
+            Console.WriteLine("'Process1' completed successfully.");
+            Console.WriteLine();
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("'Process1' encountered an issue, process aborted.");
+            Console.WriteLine(ex.Message);
+            Console.WriteLine();
+        }
+    }
+}
+
+static void Process11(String[] userEntries)
+{
+    foreach (string userValue in userEntries)
+    {
+        bool integerFormat = int.TryParse(userValue, out int valueEntered);
+
+        if (integerFormat == true)
+        {
+            if (valueEntered != 0)
+            {
+                checked
+                {
+                    int calculatedValue = 4 / valueEntered;
+                }
+            }
+            else
+            {
+                throw new DivideByZeroException("Invalid data. User input values must be non-zero values.");
+            }
+        }
+        else
+        {
+            throw new FormatException("Invalid data. User input values must be valid integers.");
+        }
+    }
+}
 Console.WriteLine("-----------------------------\n");
